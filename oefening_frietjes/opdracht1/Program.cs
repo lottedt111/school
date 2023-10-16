ef1be632926cf7788ea9f21793126155a85053a9
lottedt111 <lotte.detroyer@student.hogent.be>
2023-10-16 08:25:02 +0200

    first commit

---
+++ oefening_frietjes/opdracht1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace opdracht1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("geef een naam");
+            string input = Console.ReadLine();
+            int naam = 0;
+            naam = int.Parse(input);
+
+            Console.WriteLine("geef nog een naam");
+            string input2 = Console.ReadLine();
+            int vrouw = 0;
+            vrouw = int.Parse(input2);
+
+            Console.WriteLine("geef een gerecht");
+            string input3 = Console.ReadLine();
+            int gerecht = 0;
+            gerecht = int.Parse(input3);
+
+        }
+    }
+}
